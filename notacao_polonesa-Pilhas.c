980ee7d2078e8ee2fda69e38045e45dd36716d8a
Salomão Cruz <82367055+shl0mo@users.noreply.github.com>
2021-09-23 17:15:45 -0300

    Update notacao_polonesa-Pilhas.c

--- notacao_polonesa-Pilhas.c
+++ notacao_polonesa-Pilhas.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct no {
 	char caractere[30];
@@ -32,14 +33,19 @@ void insere_pilha(PILHAptr pilha, char caractere[30]) {
 	}
 	novo_no->prox = pilha->cabeca;
 	pilha->cabeca = novo_no;
+	pilha->tamanho = pilha->tamanho + 1;
 }
 
 void remove_pilha(PILHAptr pilha) {
 	pilha->cabeca = pilha->cabeca->prox;
+	pilha->tamanho = pilha->tamanho - 1;
 }
 
-void imprime_pilha(PILHAptr pilha) {
+void imprime_pilha(PILHAptr pilha, int i) {
 	NOptr percorre_pilha = pilha->cabeca;
+	if (i == 1) {
+		NOptr percorre_pilha = pilha->cabeca->prox;
+	}
 	while (percorre_pilha->prox != NULL) {
 		printf("%s ", percorre_pilha->caractere);
 		percorre_pilha = percorre_pilha->prox;
@@ -48,6 +54,7 @@ void imprime_pilha(PILHAptr pilha) {
 
 PILHAptr inverte_pilha(PILHAptr pilha) {
 	PILHAptr pilha_invertida = (PILHAptr) malloc(sizeof(PILHA));
+	pilha_invertida->tamanho = 0;
 	NOptr percorre_pilha = pilha->cabeca;
 	while (percorre_pilha->prox != NULL) {
 		insere_pilha(pilha_invertida, percorre_pilha->caractere);
@@ -68,47 +75,37 @@ int main(void) {
 		insere_pilha(pilha, numero_operador);
 	}
 	pilha_invertida = inverte_pilha(pilha);
-	imprime_pilha(pilha_invertida);
+	//imprime_pilha(pilha_invertida);
 	NOptr percorre_pilha = pilha_invertida->cabeca;
 	while (percorre_pilha->prox != NULL) {
-		if (percorre_pilha->caractere[0] == '#') {
+		if (percorre_pilha->prox->prox->caractere[0] == '#') {
 			float soma = 0;
+			soma = atof(percorre_pilha->caractere) + atof(percorre_pilha->prox->caractere);
 			NOptr percorre_pilha_inicio = pilha_invertida->cabeca;
-			while (percorre_pilha_inicio->caractere[0] != '#') {
-				soma = soma + atof(percorre_pilha_inicio->caractere);
+			PILHAptr pilha_provisoria = inicializa_pilha();
+			while (pilha_invertida->cabeca->caractere[0] != '#') {
 				remove_pilha(pilha_invertida);
-				percorre_pilha_inicio = pilha_invertida->cabeca;
 			}
 			remove_pilha(pilha_invertida);
 			char soma_string[30];
 			sprintf(soma_string, "%g", soma);
 			insere_pilha(pilha_invertida, soma_string);
-		} else if (percorre_pilha->caractere[0] == '-') {
-			printf("Vamos subtrair");
-			float subtracao = 0;
-			printf("ok1");
-			NOptr percorre_pilha_inicio = pilha_invertida->cabeca;
-			printf("ok2");
-			int j = 0;
-			while (percorre_pilha_inicio->caractere[0] != '-') {
-				if (j == 0) {
-					subtracao = atof(percorre_pilha_inicio->caractere) - subtracao;
-				} else {
-					subtracao = subtracao - atof(percorre_pilha_inicio->caractere);
-				}
-				remove_pilha(pilha_invertida);		
-				percorre_pilha_inicio = pilha_invertida->cabeca;
-				j++;
+			if (pilha_invertida->tamanho == 1) {
+				imprime_pilha(pilha_invertida, 0);
+				break;
 			}
-			printf("ok6");
-			remove_pilha(pilha_invertida);
-			printf("ok7");
-			char subtracao_string[30];
-			sprintf(subtracao_string, "%g", subtracao);
-			insere_pilha(pilha_invertida, subtracao_string);
+			/*
+			NOptr percorre_pilha_provisoria = pilha_provisoria->cabeca;
+			while (percorre_pilha_provisoria) {
+				//insere_pilha(pilha_invertida, percorre_pilha_provisoria->caractere);
+				percorre_pilha_provisoria = percorre_pilha_provisoria->prox;
+			}*/
+			//imprime_pilha(pilha_invertida);
+		} else {
+			
 		}
-		percorre_pilha = percorre_pilha->prox;
+	//	break;
+		percorre_pilha = pilha_invertida->cabeca;
 	}
 	printf("\n");
-	imprime_pilha(pilha_invertida);
 }
