cbc576bd8da7852d91033cf4d4b23c4056a49b09
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-19 02:10:06 -0400

    Manipulação de Arquivos, Listas, Filas e Pilhas

---
+++ Manipulação de Arquivos, Listas, Filas e Pilhas/Trab.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista{
+		
+}LISTA;
+
+typedef LISTA* LISTAptr;
+
+typedef struct pilha {
+	
+}PILHA;
+
+typedef PILHA* PILHAptr;
+
+typedef struct fila {
+	
+}FILA;
+
+typedef FILA* FILAptr;
+
+int main () {
+	printf("Manipulação de arquivos, listas, filas e pilhas");
+}
