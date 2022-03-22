5ea19d1bcbd22bfe81c97c1870e43f0a82745d9b
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-22 05:19:47 -0400

    Manipulação, Listas [...] - Reajuste do menu

--- Manipulação de Arquivos, Listas, Filas e Pilhas/Trab2.c
+++ Manipulação de Arquivos, Listas, Filas e Pilhas/Trab2.c
@@ -113,28 +113,14 @@ void lista_excluidos (LISTAptr lista, LISTAptr excluidos, char placa[6]) {
 	LISTAptr p = lista->prox;
 	while (p != NULL) {
 		int a = strcmp(p->placa, placa);
-		//printf("%i\n", a);
 		if (p->placa[0] == placa[0] && p->placa[1] == placa[1] && p->placa[2] == placa[2] && p->placa[3] == placa[3] && p->placa[4] == placa[4] && p->placa[5] == placa[5]) {
 			inserir_lista(excluidos, p->info);
 		}
 		p = p->prox;
 	}
-	/*while (p_excluidos) {
-		p = lista->prox;
-		while (p) {
-			if (p_excluidos->placa[0] == p->placa[0] && p_excluidos->placa[1] == p_placa[1] && p_excluidos->placa[2] == p_placa[2] && p_excluidos->placa[3] == p_placa[3] && p_excluidos->placa[4] == p_placa[4] && p_excluidos->placa[5] == p_placa[5]) {
-				continue;
-			} else {
-				inserir_lista(nova_lista, );
-			}
-			p = p->prox;
-		}
-		p_excluidos = p_excluidos->prox;
-	}*/
 }
 
 LISTAptr excluir (LISTAptr lista, LISTAptr excluidos) {
-	//imprimir_lista_info(excluidos);
 	char vetor_nulo[100];
 	LISTAptr nova_lista = inicializa_lista(vetor_nulo, 0);
 	LISTAptr p_excluidos = excluidos->prox;
@@ -145,14 +131,12 @@ LISTAptr excluir (LISTAptr lista, LISTAptr excluidos) {
 		p_excluidos = excluidos->prox;
 		while (p_excluidos) {
 			if (!strcmp(p_excluidos->info, p->info)) {
-				//printf("ok %s\n", p_excluidos->info);
 				excluido = 1;
 			}
 			p_excluidos = p_excluidos->prox;
 		}
 		if (!excluido) {
 			inserir_lista(nova_lista, p->info);
-			//printf("Inserir %s\n", p->info);
 		}
 		p = p->prox;
 	}
@@ -169,9 +153,6 @@ int main () {
 	LISTAptr lista = inicializa_lista(vetor_null, 0);
 	while (!feof(arq)) {
 		char linha[100];
-		/*for (i = 0; i < 100; i++) {
-			linha[i] = ' ';
-		}*/
 		fgets(linha, 100, arq);
 		if (linha[0] != '#' && linha[0] != ' ') {
 			inserir_lista(lista, linha);
@@ -183,8 +164,8 @@ int main () {
 		do {
 			printf("Escolha uma das opções abaixo: \n\n");
 			printf("1 - Inserção\n");
-			printf("2 - Busca\n");
-			printf("3 - Exclusão\n");
+			printf("2 - Exclusão\n");
+			printf("3 - Busca\n");
 			printf("4 - Relatório\n");
 			printf("5 - Sair\n");
 			scanf("%i", &escolha);
@@ -194,12 +175,12 @@ int main () {
 		}
 		if (escolha == 1) {
 			printf("Informe os dados a serem inseridos\n");
-			char dados[100], c;
+			char dados[100];
 			int i = 0;
 			scanf(" %99[^\n]", dados);
 			inserir_lista(lista, dados);
 			inserir_lista(lista_inicial, dados);
-		} else if (escolha == 2) {
+		} else if (escolha == 3) {
 			escolha = 0;
 			do {
 				printf("\nEscolha o tipo de busca: \n\n");
@@ -207,7 +188,13 @@ int main () {
 				printf("2 - Busca por fila\n");
 				scanf("%i", &escolha);
 			} while (escolha < 1 || escolha > 2);
-		} else if (escolha == 3) {
+			printf("Escolha todos os dados do veículo a ser buscado\n");
+			if (escolha == 1) {
+					
+			} else {
+					
+			}
+		} else if (escolha == 2) {
 			printf("Informe todos os dados do veículo que deseja excluir\n");
 			char placa[6];
 			scanf("%s", placa);
