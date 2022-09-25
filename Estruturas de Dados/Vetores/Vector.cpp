12e323d0dfd7894da4dd0f46c636f45d61e2d9a3
shl0mo <salomao.dacostacruz@gmail.com>
2022-09-25 18:06:07 -0400

    Vetor - Vetor.cpp - copia_palavra()

--- Estruturas de Dados/Vetores/Vector.cpp
+++ Estruturas de Dados/Vetores/Vector.cpp
@@ -1,17 +1,17 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct vetor {
+typedef struct Loja {
 	char tipo[100];
 	char marca[100];
 	char modelo[100];
 	double valor;
 	int quantidade_estoque;
 	double valor_frete;
-	struct vetor* prox;
-} VETOR;
+	struct Loja* prox;
+} LOJA;
 
-typedef VETOR* VETORptr;
+typedef LOJA* LOJAptr;
 
 void apaga_palavra(char palavra[100]) {
 	int i;
@@ -20,9 +20,19 @@ void apaga_palavra(char palavra[100]) {
 	}
 }
 
-VETORptr inicializa_vetor () {
-	VETORptr novo_vetor = VETORptr malloc(sizeof(VETOR));
+void copia_palavra(char vetor_loja[100], char palavra[100]) {
+	int i;
+	for (i = 0; i < 100; i++) {
+		vetor_loja[i] = palavra[i];
+	}
+}
+
+LOJAptr insere_no_loja (char tipo[100], char marca[100], char modelo[100], int quantidade_estoque, int valor_frete) {
+	LOJAptr novo_no = LOJAptr malloc(sizeof(LOJA));
 	
+	novo_no->valor = 0;
+	novo_no->quantidade_estoque = 0;
+	novo_no->valor_frete = 0;
 	return novo_vetor;
 }
 
