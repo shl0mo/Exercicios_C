958589540ee6af1273dd35fd32cb0dfdf0dd756c
shl0mo <salomao.dacostacruz@gmail.com>
2022-09-25 20:28:14 -0400

    Vetor - Vetor.cpp - insere_no_loja()

--- Estruturas de Dados/Vetores/Vector.cpp
+++ Estruturas de Dados/Vetores/Vector.cpp
@@ -27,13 +27,25 @@ void copia_palavra(char vetor_loja[100], char palavra[100]) {
 	}
 }
 
-LOJAptr insere_no_loja (char tipo[100], char marca[100], char modelo[100], int quantidade_estoque, int valor_frete) {
+LOJAptr insere_no_loja (LOJAptr loja, int primeiro; char tipo[100], char marca[100], char modelo[100], double valor, int quantidade_estoque, double valor_frete) {
 	LOJAptr novo_no = LOJAptr malloc(sizeof(LOJA));
-	
-	novo_no->valor = 0;
-	novo_no->quantidade_estoque = 0;
-	novo_no->valor_frete = 0;
-	return novo_vetor;
+	copia_palavra(novo_no->tipo, tipo);
+	copia_palavra(novo_no->marca, marca);
+	copia_palavra(novo_no->modelo, modelo);
+	novo_no->valor = valor;
+	novo_no->quantidade_estoque = quantidade_estoque;
+	novo_no->valor_frete = valor_frete;
+	if (primeiro == 1) {
+		novo_no->prox = NULL;
+		return novo_vetor;
+	}
+	LOJAptr p = loja;
+	while (p->prox != NULL) {
+		p = p->prox;
+	}
+	p->prox = novo_vetor;
+	novo_vetor->prox = NULL;
+	return NULL;
 }
 
 int main () {
